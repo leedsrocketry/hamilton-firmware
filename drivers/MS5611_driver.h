960b00ac0ae7f0f601278e870f7005f5a44ddb39
AlexandraPosta <posta_alex222@yahoo.com>
2023-06-11 17:39:03 +0100

    Initial driver for Barometer

--- drivers/MS5611_driver.h
+++ drivers/MS5611_driver.h
@@ -6,7 +6,54 @@
   Description: header file for the Barometer module MS561101BA03
 */
 
+#ifndef _MS5611_H
 #define MS5611_DRIVER_H
-
 #include <stdint.h>
 
+#define MS5611_RESET			      0x1e
+#define MS5611_READ_ADC			    0x00
+#define MS5611_READ_PROM_WORD		0xA0
+#define MS5611_PROM_WORDS_NB		8
+#define	SPI_MODE_0		(0|0)		/* (original MicroWire) */
+
+
+/**
+  @brief OverSampling Rate descriptor.
+  @warning: cmd MUST be kept aligned on a word boundary (see m5611_spi_read_adc_temp_and_pressure in ms5611_spi.c).
+*/
+typedef struct OversampleRate {
+	unsigned long conv_usec;
+	uint8_t cmd;
+	unsigned short rate;
+} OversampleRate;
+
+
+/**
+  @brief TODO
+*/
+typedef struct State {
+  void *client;
+	struct mutex lock;
+
+	const struct OversampleRate pressure_osr;
+	const struct OversampleRate temp_osr;
+
+	uint16_t prom[MS5611_PROM_WORDS_NB];
+
+	int (*reset)(State *st);
+	int (*read_prom_word)(State *st, int index, u16 *word);
+	int (*read_adc_temp_and_pressure)(State *st,
+					  s32 *temp, s32 * pressure);
+
+	int (*compensate_temp_and_pressure)(State *st, s32 *temp,
+					  s32 *pressure);
+} State;
+
+
+/**
+  @brief TODO
+*/
+int probe();
+
+
+#endif /* _MS5611_H */
