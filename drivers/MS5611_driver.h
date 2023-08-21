e82f5616a8b927843ffc20087e3097df67df7518
Alexandra Posta <posta_alex222@yahoo.com>
2023-08-21 14:48:13 +0100

    Latest changes drivers

--- drivers/MS5611_driver.h
+++ drivers/MS5611_driver.h
@@ -10,7 +10,6 @@
 #include <stdint.h>
 
 #pragma region Macros
-
 /** @name Commands */
 #define MS5611_CMD_READ_ADC       				0x00
 #define MS5611_CMD_READ_PROM      				0xA0
@@ -31,6 +30,11 @@
 #define MS5611_TEMP_OSR_2048     				0x56
 #define MS5611_TEMP_OSR_4096     				0x58
 
+#define MS5611_CONVERSION_OSR_256  				1
+#define MS5611_CONVERSION_OSR_512  				2
+#define MS5611_CONVERSION_OSR_1024 				3
+#define MS5611_CONVERSION_OSR_2048 				5
+#define MS5611_CONVERSION_OSR_4096 				10
 #pragma endregion Macros
 
 #pragma region Structs/Emun
@@ -48,9 +52,48 @@ typedef enum MS5611_OSR {
 #pragma endregion Structs/Emun
 
 /**
-  @brief TODO
+  @brief Initialise the pressure sensor with default parameters
 */
 int_8 init_MS5611();
 
+
+/**
+  @brief Set the OSR (Oversampling rate). Setting another value from the enumeration will put the min OSR
+  @note Setting an higher value means longer time to read the data
+  @param osr The oversampling rate
+*/
+void MS5611_set_OSR (MS5611_OSR osr);
+
+
+/**
+	@brief Return the temperature with a 2 digits precision in celcius. 
+	@note Example : 2000 -> 20,00°C
+	@param calculate True if value needs to be updated. It will update the threes values
+	@return The temperature in Celcius
+*/
+int32_t MS5611_get_temperature(bool calculate);
+
+
+/**
+	@brief Return the pressure in mbar with a 2 digits precision
+	@note Example : 100000 -> 1000,00 mbar
+	@param calculate True if value needs to be updated. It will update the threes values
+	@return The pressure in mbar
+*/
+int32_t MS5611_get_pressure(bool calculate);
+
+
+/**
+	@brief Return the altitude in meters
+	@param calculate True if value needs to be updated. It will update the threes values
+	@return The altitude in meters
+*/
+int32_t MS5611_get_altitude(bool calculate);
+
+
+/**
+	@brief Calculate/update the altitude/pressure/temperature using the barometer
+*/
+void MS5611_calculate();
 
 #endif /* MS5611_DRIVER_H */
