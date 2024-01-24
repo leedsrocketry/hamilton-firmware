9cd5713e1615ec59c300c38c7d733ac51683b50f
el21tg <el21tg@leeds.ac.uk>
2024-01-24 11:52:33 +0000

    feat(drivers): inital Pad radio Driver

--- drivers/SI446_driver.c
+++ drivers/SI446_driver.c
@@ -7,10 +7,137 @@
 
 #include "SI446_driver.h"
 
-int8_t init_SI446(){
 
+// __________________________________ Public _________________
+
+int8_t init_SI446(SPI_Type_Def *spi){
+    int8_t retVal;
+    SI446_SPI = spi;
+
+    return retVal;
 };
 
-int8_t get_data_SI446(SI446_data *data){
-
+void Write_data_SI446(uint8_t *data, size_t byteCount){
+    spi_write_byte(SI446_SPI, SI446_WRITE_TX_FIFO_CMD);
+    spi_write_buf(SI446_SPI, data, byteCount);
 };
+
+void Read_data_SI446(uint8_t *data, size_t byteCount){
+    spi_write_byte(SI446_SPI, SI446_WRITE_RX_FIFO_CMD);
+    spi_ready_read(SI446_SPI);
+    for(int i = 0; i < byteCount; i ++){
+        data[i] = spi_read_byte(SI446_SPI);
+    }
+};
+
+int8_t Transmit_SI446(){
+    int8_t retVal;
+    uint8_t command[4];
+    command[0] = SI446_START_TX_CMD;
+    command[1] = SI446_Radio_settings.SI446_channel;
+    command[2] = 00110000;   // go into ready state, send data currently in FIFO, imeadiately
+    command[3] = 0; // use the packet handler packet config.
+    spi_write_buf(SI446_SPI, command, 4);
+    retVal = check_CTS(-1)   // CTS returned once chip is in TX state - doesnt mean packet has been sent
+    return retVal;
+}
+
+int8_t Recieve_SI446(){
+    int8_t retVal;
+    uint8_t command[7];
+    command[0] = SI446_START_RX_CMD;
+    command[1] = SI446_Radio_settings.SI446_channel;
+    command[2] = 1;  // go into RX state imeadiately
+    command[3] = 0; // use the packet handler packet config.
+    command[4] = SI446_Radio_settings.SI446_RXTIME_OUT_STATE;
+    command[5] = SI446_Radio_settings.SI446_RX_VALID_STATE;
+    command[6] = SI446_Radio_settings.SI446_RX_INVALID_STATE;
+    spi_write_buf(SI446_SPI, command, 7);
+    retVal = check_CTS(-1)   // CTS returned once chip is in TX state - doesnt mean packet has been sent
+    return retVal;
+}
+
+
+//_________________________________ Private ____________________
+
+/**
+  @brief funtion to deal with recieving data from the sensor
+  @note waits until CTS is correct then records data
+  @param byteCount number of bytes to clock and read
+  @param data ptr to the data storage location
+  @return Success/Failure
+*/
+static int8_t get_response(int byteCount, uint8_t *data){
+    int8_t retVal;
+    // wait till CTS is correct
+    retVal = check_CTS(0)
+    if(retVal){
+        for(int i = 0; i < byteCount; i ++){
+            data[i] = spi_read_byte(SI446_SPI);
+        }
+        retVal = 1;
+    } // CTS not correct
+    return retVal;
+};
+
+/**
+  @brief checks if CTS bit is set
+  @note e.g. used to check if command with no response has been correctly carried out
+  @param desired -1: no time_out, 
+                  0: default (SI446_CTS_TIME_OUT)
+                 >0: custom time (in ms) to wait for CTS response.
+  @return Success/Failure
+*/
+static int8_t check_CTS(int desired){
+    int time_out = SI446_CTS_TIME_OUT;
+    if(desired > 0){ // use user defined  
+    time_out = desired
+    }
+    else if(desired < 0){   // no timeout needed
+        spi_ready_ready(SI446_SPI); // wait till buffer not empty 
+        if(spi_read_byte(SI446_SPI) == 0xFF){ 
+            return 1; // CTS ready
+        };
+        return SI446_E_CTS_INVALID;
+    }
+    // check for time out
+    while(time_out > 0){
+        if((SI446_SPI->SR & BIT(1)) && (SI446_SPI->SR & BIT(0))){ // there is data
+            if(spi_read_byte(SI446_SPI) == 0xFF){
+                return 1;
+            }
+            else{
+                // NEED TO SEND NSELC HIGH AND 
+                return SI446_E_CTS_INVALID;
+            }
+        }
+        else{
+            delay(1);   // wait 1ms
+            time_out --;
+        }
+    }
+    return SI446_E_CTS_TIME_OUT;
+};
+
+/**
+  @brief COMMAND: power up the sensor with specified function
+  @return Success/Failure
+*/
+static int8_t Power_up(){
+    int8_t retVal;
+    uint8_t packet[7];
+    uint32_t xo_freq = 30000000; // default
+
+    packet[0] = SI446_POWER_UP_CMD;
+    packet[1] = 1;
+    packet[2] = 0;
+    packet[3] = (uint8_t)(xo_freq >> 24);
+    packet[4] = (uint8_t)(xo_freq >> 16);
+    packet[5] = (uint8_t)(xo_freq >> 8);
+    packet[6] = (uint8_t)(xo_freq);
+    spi_write_buf(SI446_SPI, packet, 7);
+    retVal = check_CTS(50); // ensure it has been processed correctly..takes longer
+    return retVal;
+};
+
+
--- drivers/SI446_driver.h
+++ drivers/SI446_driver.h
@@ -4,34 +4,167 @@
   Created on: 10 June 2023
   Description: header file for the Pad Radio module SI446
 */
+
+
+// couple of questions?????????
+// what exactly is the spi write/read function we be using, how to link it here
+// what radio information do we want - mode, freq, power, rssi
+// how can I do the CTS checking?
+// there are some quick access registers - do we need to set them up
+// any update on the ATMega MCU attached to the gpio?
+// any interupts wont be seen via spi - only on interuppt pin 
+
+
 #ifndef SI446_DRIVER_H
 #define SI446_DRIVER_H
+#include "mcu.h"
 
+#pragma region Macros //_______________ MACROS _______________________
+
+/** @name Constants */
+#define SI446_CTS_TIME_OUT                                  20
+
+/** @name SI446 Machine States */
+#define SI446_NO_CHANGE_STATE                               0
+#define SI446_SLEEP_STATE                                   1
+#define SI446_SPI_ACTIVE_STATE                              2
+#define SI446_READY_STATE                                   3
+#define SI446_NO_ANT_READY_STATE                            4
+#define SI446_TX_TUNE_STATE                                 5
+#define SI446_RX_TUNE_STATE                                 6
+#define SI446_TX_STATE                                      7
+#define SI446_RX_STATE                                      8
+
+/** @name SI446 chip Status */
+#define SI446_OK                                            0
+#define SI446_E_NULL_PTR                                    -1
+#define SI446_E_CTS_INVALID                                 -2
+#define SI446_E_CTS_TIME_OUT                                -3       
+#define SI446_E_BAD_COMMAND                                 -4
+#define SI446_E_INVALID_CMD_ARGS                            -5
+#define SI446_E_INVALID_CMD_ISSUE                           -6
+#define SI446_E_FIFO_UNDERFLOW_OVERFLOW                     1 // not sure
+
+/** @name Chip settings */
+// found in "AN625: Si446x API Descriptions"
+
+
+
+
+/** @name Commands */
+// found in "AN625: Si446x API Descriptions"
+#define SI446_POWER_UP_CMD                                  0x02
+#define SI446_SET_PROPERTY_CMD                              0x11
+#define SI446_GET_PROPERTY_CMD                              0x12
+#define SI446_GET_CHIP_STATUS                               0x23
+#define SI446_START_TX_CMD                                  0x31
+#define SI446_START_RX_CMD                                  0x32
+#define SI446_WRITE_TX_FIFO_CMD                             0x66
+#define SI446_READ_RX_FIFO_CMD                              0x77
+#define SI446_FIFO_INFO                                     0x15
+
+/** @name properties registers */
+//#define SI446_ 
+
+SPI_TypeDef SI446_SPI;
+
+#pragma endregion Macros
+
+#pragma region Structs/Emun //_______________ STRUCTS _______________________
+
+typedef struct SI446_chip_settings
+{
+  uint32_t SI446_XO_FREQUENCY;
+  uint8_t BOOT_OPTIONS;
+  uint8_t BOOT_OPTIONS;
+} SI446_chip_settings;
+
+/**
+ * @brief SI446 radio settings
+ */
+typedef struct SI446_Radio_settings
+{
+  uint8_t SI446_channel;
+  uint8_t SI446_CURRENT_RSSI;
+
+  uint8_t SI446_RX_TIMEOUT_STATE;
+  uint8_t SI446_RX_VALID_STATE;
+  uint8_t SI446_RX_INVALID_STATE;
+
+} SI446_Radio_settings;
+
+/**
+ * @brief SI446 radio settings
+ */
+typedef struct SI446_Packet_settings
+{
+  uint8_t SI446_channel
+
+} SI446_Packet_settings;
+
+/**
+ * @brief SI446 transmit data structure when rocket in flight ?
+ */
 typedef struct SI446_data
 {
+  uint8_t altitude;
+  uint8_t batteryLevel;
+  uint8_t altitude_barom;
+  uint8_t gnss_x_coord;
+  uint8_t gnss_y_coord;
+  uint8_t gnss_z_coord;
+} SI446_Packet_settings;
 
-} SI446_data;
+#pragma endregion Structs/Emun
 
-#pragma region Public
-
+#pragma region Public //_________________ PUBLIC _________________
 /**
-  @brief Initialization of the SI4463 module
+  @brief Initialization of the SI446 module
   @note
   @return Success/Failure
 */
-int8_t init_SI446();
+int8_t init_SI446(SPI_TypeDef spi);
 
 /**
-  @brief Get data from the SI446 Pad Radio module
+  @brief loads the data into the FIFO ready for transmittion
   @note
-  @param data ptr to SI446_data struct for returning data
-  @return Success/Failure
+  @param data ptr to the data which is to be sent
 */
-int8_t get_data_SI446(SI446_data *data);
+void Write_data_SI446(uint8_t *data);
+
+/**
+  @brief reads recieved data from the FIFO buffer
+  @note
+  @param data ptr to the data which is to be sent
+*/
+void Read_data_SI446(uint8_t *data);
+
+/**
+  @brief send SI446 into recieve mode using current radio settings
+  @note
+*/
+int8_t Recieve_SI446();
+
+/**
+  @brief send SI446 into transmit mode using current radio settings
+  @note
+*/
+int8_t Transmit_SI446();
+
 
 #pragma endregion Public
 
-#pragma region Private
+
+#pragma region Private  //_________________ PRIVATE _________________
+/**
+  @brief send a command to the SI4460 sensor
+  @note doesnt deal with the response
+  @param byteCount number of bytes in the command to be sent (includes command)
+  @param data pointer to the command parameters which are to be sent
+  @return Success/Failure
+*/
+
+
 #pragma endregion Private
 
 #endif /* SI446_DRIVER_H */
