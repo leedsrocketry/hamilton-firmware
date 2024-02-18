8eef076218054cf9248fa1309ec1262bbfe048c6
el21tg <el21tg@leeds.ac.uk>
2024-02-18 23:18:44 +0000

    feat(LSM_driver): first version completed

--- drivers/LSM6DS3_driver.c
+++ drivers/LSM6DS3_driver.c
@@ -1,23 +1,75 @@
 /*
     Leeds University Rocketry Organisation - LURA
-    Author Name: Alexandra Posta
+    Author Name: Tyler Green
     Created on: 01 February 2024
     Description: Driver file for the IMU module LSM6DS3 (https://www.mouser.co.uk/datasheet/2/389/dm00133076-1798402.pdf)
 */
 
 #include "LSM6DS3_driver.h"
 
-#pragma region Public
 
-int8_t LSM6DS3_init(){
+SPI_TypeDef *LSM6DS3_SPI;
+#pragma region Public   //___________ Public _________________
 
+int8_t LSM6DS3_init(SPI_TypeDef *spi){
+    int8_t LSM6DS3_retVal = 123;
+    LSM6DS3_SPI = spi;
+    // check if we recieve who am I correctly
+    LSM6DS3_retVal = LSM6DS3_read_reg(LSM6DS3_WHO_AM_I_ADRS);
+    if(LSM6DS3_retVal == LSM6DS3_WHO_AM_I_EXP){
+        printf("LSM6DS3 knows who it is...\n");
+        // power on the accelerometer
+        LSM6DS3_write_reg(LSM6DS3_CTRL1_XL_ADRS, 01000000);
+        LSM6DS3_write_reg(LSM6DS3_CTRL2_G_ADRS, 01000000)
+    }
+    else{
+        LSM6DS3_retVal = -1;
+    }
+    return LSM6DS3_retVal;
 };
 
 int8_t LSM6DS3_get_data(LSM6DS3_data *data){
-
+    int8_t LSM6DS3_retVal;
+    spi_enable_cs(LSM6DS3_SPI, LSM6DS3_CS);
+    spi_write_byte(LSM6DS3_SPI, LSM6DS3_TEMP_ADDRESS);
+    data->LSM6DS3_TEMP = LSM6DS3_get_sens_val();
+    data->LSM6DS3_GYRO_X = LSM6DS3_get_sens_val();
+    data->LSM6DS3_GYRO_Y = LSM6DS3_get_sens_val();
+    data->LSM6DS3_GYRO_Z = LSM6DS3_get_sens_val();
+    data->LSM6DS3_ACC_X = LSM6DS3_get_sens_val();
+    data->LSM6DS3_ACC_Y = LSM6DS3_get_sens_val();
+    data->LSM6DS3_ACC_Z = LSM6DS3_get_sens_val();
+    spi_disable_cs(LSM6DS3_SPI, LSM6DS3_CS);
+    return 1;
 };
 
 #pragma endregion Public
 
 #pragma region Private
+
+static int16_t LSM6DS3_get_sens_val(){
+    int16_t val1, val2;
+    val1 = (int16_t)spi_read_byte(LSM6DS3_SPI);
+    val2 = (int16_t)spi_read_byte(LSM6DS3_SPI) << 8;
+    return val1 && val2;
+};
+
+static uint8_t LSM6DS3_write_reg(uint8_t address, uint8_t data){
+    spi_enable_cs(LSM6DS3_SPI,LSM6DS3_CS);
+    spi_write_byte(LSM6DS3_SPI,address);
+    spi_write_byte(LSM6DS3_SPI,data);
+    spi_disable_cs(LSM6DS3_SPI,LSM6DS3_CS);
+};
+
+static uint8_t LSM6DS3_read_reg(uint8_t address){
+    address &= 0x80;   // make sure first bit is 1 
+    uint8_t LSM6DS3_retVal = 123;
+    spi_enable_cs(LSM6DS3_SPI,LSM6DS3_CS);
+    spi_write_byte(LSM6DS3_SPI,address);
+    LSM6DS3_retVal = spi_read_byte(LSM6DS3_SPI);
+    spi_disable_cs(LSM6DS3_SPI,LSM6DS3_CS);
+    return LSM6DS3_retVal;
+};
+
+
 #pragma endregion Private
--- drivers/LSM6DS3_driver.h
+++ drivers/LSM6DS3_driver.h
@@ -6,11 +6,43 @@
 */
 #ifndef LSM6DS3_DRIVER_H
 #define LSM6DS3_DRIVER_H
+#include "mcu.h"
+
+#pragma region Macros //_______________ MACROS _______________________
+//register addresses
+#define LSM6DS3_WHO_AM_I_ADRS                0x0F
+
+#define LSM6DS3_CTRL1_XL_ADRS                0x10
+#define LSM6DS3_CTRL2_G_ADRS                 0x11
+
+#define LSM6DS3_TEMP_ADDRESS                 0x20
+
+//chip settings
+#define LSM6DS3_WHO_AM_I_EXP                 01101001 //expected val if who reg is read
+#define LSM6DS3_CTRL1_XL_ON                  01000000
+#define LSM6DS3_CTRL2_G_ON                   01000000
+
+#define LSM6DS3_CS                           0 // ???????????????
+
+#pragma endregion Macros
+
+#pragma region Structs/Emun //_______________ STRUCTS _______________________
 
 typedef struct LSM6DS3_data
 {
+  int16_t LSM6DS3_TEMP;
+
+  int16_t LSM6DS3_GYRO_X;
+  int16_t LSM6DS3_GYRO_Y;
+  int16_t LSM6DS3_GYRO_Z; 
+
+  int16_t LSM6DS3_ACC_X;
+  int16_t LSM6DS3_ACC_Y;
+  int16_t LSM6DS3_ACC_Z;
 
 } LSM6DS3_data;
+
+#pragma endregion Structs/Emun
 
 #pragma region Public
 
@@ -19,7 +51,7 @@ typedef struct LSM6DS3_data
   @note
   @return Success/Failure
 */
-int8_t init_LSM6DS3();
+int8_t LSM6DS3_init();
 
 /**
  * @brief Get data from the LSM6DS3 IMU module
@@ -27,11 +59,35 @@ int8_t init_LSM6DS3();
  * @param data ptr to LSM6DS3_data struct for returning data
  * @return Success/Failure
  */
-int8_t get_data_LSM6DS3(LSM6DS3_data *data);
+int8_t LSM6DS3_get_data(LSM6DS3_data *data);
 
 #pragma endregion Public
 
 #pragma region Private
+/**
+ * @brief write to a specific register
+ * @note
+ * @param address the register to write to
+ * @param data data to send
+ * @return Success/Failure
+ */
+static uint8_t LSM6DS3_write_reg(uint8_t address, uint8_t data);
+
+/**
+ * @brief Get a value from a register
+ * @note
+ * @param address the register to write to
+ * @return the value read
+ */
+static uint8_t LSM6DS3_read_reg(uint8_t address);
+
+/**
+ * @brief extract and combine results from sensor registors
+ * @note  doesnt deal with CS - purely for public get_data function
+ * @return Success/Failure
+ */
+static int16_t LSM6DS3_get_sens_val();
+
 #pragma endregion Private
 
 #endif /* LSM6DS3_DRIVER_H */
