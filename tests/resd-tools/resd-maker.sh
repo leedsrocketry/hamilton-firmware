fa6a27b7bed7b6ff87b842fce8e2cd5af02f7ec3
EvanM <evan.madurai@googlemail.com>
2024-09-30 11:02:59 +0100

    test: testing renode settings

--- tests/resd-tools/resd-maker.sh
+++ tests/resd-tools/resd-maker.sh
@@ -1,8 +1,8 @@
 ./csv2resd.py \
-    -i acc_test.csv \
+    -i accel_2.csv \
         -m acceleration:accel_x,accel_y,accel_z:x,y,z:0 \
         -s 0 \
-        -f 1 \
+        -f 1000 \
     output.resd
 
 mv output.resd ../../renode/output.resd
