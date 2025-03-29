9a6adcb79465c9a46509c28eee6f98750611bdce
EvanMad <evan.madurai@gmail.com>
2025-03-29 19:40:41 +0000

    changes + data

--- src/flight_manager.c
+++ src/flight_manager.c
@@ -18,29 +18,39 @@ void set_flight_stage(FlightStage fs) { flightStage = fs; }
 double max_altitude = 0;
 double ground_altitude = 0;
 
-void handle_LAUNCHPAD(Frame *frame) {
+void handle_LAUNCHPAD(Frame *frame)
+{
   double altitude =
       barometric_equation(frame->barometer.pressure, frame->barometer.temp);
+  
+  printf_float("altitude", (float)altitude, true);
+  printf("\r\n");
 
-  if (altitude > (ground_altitude + ALTITUDE_APOGEE_THRESHOLD)) {
-    LOG("LAUNCHPAD: Altitude threshold met\r\n");
-    flightStage = ASCENT;
-    return;
-  }
+  // if (altitude > (ground_altitude + ALTITUDE_APOGEE_THRESHOLD)) {
+  //   LOG("LAUNCHPAD: Altitude threshold met\r\n");
+  //   flightStage = ASCENT;
+  //   return;
+  // }
 
-  if (frame->accel.x < ACCEL_LAUNCH_THRESHOLD) {
+  if (frame->accel.x < ACCEL_LAUNCH_THRESHOLD)
+  {
     LOG("LAUNCHPAD: Acceleration threshold met\r\n");
     flightStage = ASCENT;
     return;
   }
 }
 
-void handle_ASCENT(Frame *frame) {
+void handle_ASCENT(Frame *frame)
+{
   double altitude =
       barometric_equation(frame->barometer.pressure, frame->barometer.temp);
 
-  if (altitude < (max_altitude - ALTITUDE_APOGEE_THRESHOLD)) {
-    LOG("LAUNCHPAD: Acceleration threshold met\r\n");
+  printf_float("altitude", (float)altitude, true);
+  printf("\r\n");
+
+  if (altitude < (max_altitude - ALTITUDE_APOGEE_THRESHOLD))
+  {
+    LOG("ASCENT: Altitude threshold met\r\n");
     flightStage = DESCENT;
   }
 }
@@ -52,7 +62,8 @@ void handle_DESCENT(Frame *frame) { (void)frame; }
 
 void handle_LANDING(Frame *frame) { (void)frame; }
 
-void run_flight() {
+void run_flight()
+{
   init_flash();
 
   Frame init_frame;
@@ -63,38 +74,41 @@ void run_flight() {
 
   CircularBuffer *cb = cb_create(20);
 
-  for (;;) {
+  for (;;)
+  {
     Frame frame;
     read_sensors(&frame);
     int8_t write_success = save_frame(frame);
-    if (write_success != SUCCESS) {
+    if (write_success != SUCCESS)
+    {
       LOG("WRITE FAILED\r\n");
     }
 
     cb_enqueue_overwrite(cb, &frame);
     Frame avg_frame;
     cb_average(cb, &avg_frame);
-    print_sensor_line(avg_frame);
+    // print_sensor_line(avg_frame);
 
     // LOG("Flight stage: %d\r\n", flightStage);
-    switch (flightStage) {
-      case LAUNCHPAD:
-        handle_LAUNCHPAD(&avg_frame);
-        break;
-      case ASCENT:
-        handle_ASCENT(&avg_frame);
-        break;
-      case APOGEE:
-        handle_APOGEE(&avg_frame);
-        break;
-      case DESCENT:
-        handle_DESCENT(&avg_frame);
-        break;
-      case LANDING:
-        handle_LANDING(&avg_frame);
-        break;
-      default:
-        break;
+    switch (flightStage)
+    {
+    case LAUNCHPAD:
+      handle_LAUNCHPAD(&avg_frame);
+      break;
+    case ASCENT:
+      handle_ASCENT(&avg_frame);
+      break;
+    case APOGEE:
+      handle_APOGEE(&avg_frame);
+      break;
+    case DESCENT:
+      handle_DESCENT(&avg_frame);
+      break;
+    case LANDING:
+      handle_LANDING(&avg_frame);
+      break;
+    default:
+      break;
     }
   }
 }
